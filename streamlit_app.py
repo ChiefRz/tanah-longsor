6b5211c78ecf2ac028aa3da3b89b0156fed15ccc
ChiefRz <151807591+ChiefRz@users.noreply.github.com>
2024-06-03 03:57:19 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ with st.sidebar:
     selected_year = st.selectbox('Select a year', year_list)
     df_rekap_selected_year = all_rekap[all_rekap.TAHUN == selected_year]
     df_peta_selected_year = all_peta[all_peta.TAHUN == selected_year]
-    df_selected_year_sorted = all_peta[all_peta.TAHUN == selected_year].sort_values(by="KEJADIAN", ascending=False)
+    df_selected_year_sorted = df_peta_selected_year.sort_values(by="KEJADIAN", ascending=False)
 
 #######################
 # Plots
