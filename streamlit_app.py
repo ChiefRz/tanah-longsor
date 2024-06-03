22b55c4b0fec306fe1f8cfcf4aeb17666f59f1f5
ChiefRz <151807591+ChiefRz@users.noreply.github.com>
2024-06-03 10:33:40 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,7 +65,7 @@ with col[0]:
     fig.update_geos(fitbounds="locations", visible=True)
     fig.show()
 
-    fig = px.line(df_rekap_selected_year, x=df_rekap_selected_year.index, y=TANGGAL_KEJADIAN)
+    fig = px.line(df_rekap_selected_year, x=df_rekap_selected_year.index, y='TANGGAL_KEJADIAN')
     st.plotly_chart(fig, use_container_width=True)
     
 with col[1]:
