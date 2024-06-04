fec81db2bd875eff524c7576f729e8313d68c3ce
ChiefRz <151807591+ChiefRz@users.noreply.github.com>
2024-06-04 21:49:14 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -94,7 +94,10 @@ def process_data(input):
     # Tampilkan bulan yang tidak memiliki value 
     all_bulan = pd.DataFrame(nama_bulan, columns=["Bulan"])
     jumlah_kejadian = jumlah_kejadian.merge(all_bulan, how="outer").fillna(0)
-    jumlah_kejadian = jumlah_kejadian.reset_index()
+    
+    month_order = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    jumlah_kejadian["Bulan"] = pd.Categorical(jumlah_kejadian["Bulan"], categories=month_order, ordered=True)
+    jumlah_kejadian = jumlah_kejadian.sort_values("Bulan")
     
     return jumlah_kejadian
 
