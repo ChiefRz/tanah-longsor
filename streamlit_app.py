801ed05dfec7ad09298c994cb109b18e5ecdf1fd
ChiefRz <151807591+ChiefRz@users.noreply.github.com>
2024-06-04 21:45:50 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -88,12 +88,12 @@ def process_data(input):
     jumlah_kejadian.index = [nama_bulan[i-1] for i in jumlah_kejadian.index]
     
     # Ubah indeks menjadi kolom biasa dan membuat indeks baru
-    jumlah_kejadian = jumlah_kejadian.reset_index()
     jumlah_kejadian = jumlah_kejadian.rename(columns={"index": "Bulan", "NO": "Jumlah Kejadian Bencana"})
 
     # Tampilkan bulan yang tidak memiliki value 
     all_bulan = pd.DataFrame(nama_bulan, columns=["Bulan"])
     jumlah_kejadian = jumlah_kejadian.merge(all_bulan, how="outer").fillna(0)
+    jumlah_kejadian = jumlah_kejadian.reset_index()
     
     return jumlah_kejadian
 
