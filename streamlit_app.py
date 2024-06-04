f41822829464568ba8c3d02a7c9c8706233d6a59
ChiefRz <151807591+ChiefRz@users.noreply.github.com>
2024-06-04 10:11:26 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ def make_choropleth(input_df, input_js, input_id, input_columne):
                                color=input_columne,
                                color_continuous_scale='Reds',
                                range_color=(0, max(df_peta_selected_year.KEJADIAN)),
-                               labels={'KEJADIAN':'KEJADIAN'}
+                               labels={'KEJADIAN':'KEJADIAN'},
                                hover_data='KECAMATAN'
                                )
     choropleth.update_layout(
