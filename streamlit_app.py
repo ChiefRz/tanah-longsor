c74c0f6178bd8f9827548cbbeeafafff152ffeec
ChiefRz <151807591+ChiefRz@users.noreply.github.com>
2024-06-04 09:48:52 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,7 @@ col = st.columns((5, 2), gap='medium')
 with col[0]:
     st.markdown(f' #### Peta Sebaran Tanah Longsor Kab. Semarang pada Tahun {selected_year}')
 
-    fig = px.choropleth(df_peta_selected_year, geojson=df_peta_selected_year.geometry, locations='KECAMATAN', color='KEJADIAN', 
+    fig = px.choropleth(df_peta_selected_year, locations='KECAMATAN', color='KEJADIAN', 
                                color_continuous_scale='Reds',
                                range_color=(0, max(df_peta_selected_year.KEJADIAN)),
                                labels={'KEJADIAN':'KEJADIAN'}
