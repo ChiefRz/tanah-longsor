a1ea07540e26854883cfd65cd3384e0bd671bd1c
ChiefRz <151807591+ChiefRz@users.noreply.github.com>
2024-06-04 20:57:14 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,6 +55,7 @@ def make_choropleth(input_df, input_js, input_id, input_columne):
     return choropleth
 
 def tren(input_df):
+    df_rekap_selected_year.set_index(df_rekap_selected_year.TANGGAL_KEJADIAN, inplace=True)
     rekap_ts = input_df.resample(rule='M', on=df_rekap_selected_year.TANGGAL_KEJADIAN).agg({
         "NO": "nunique"
     })
