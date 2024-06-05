07dabdd37dda3ec2e470c6d3f64664524a03d39a
ChiefRz <151807591+ChiefRz@users.noreply.github.com>
2024-06-05 09:20:19 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,15 +47,13 @@ def make_choropleth(input_df, input_js, input_id, input_columne):
                                       labels={'KEJADIAN':'KEJADIAN'},
                                       hover_name='KECAMATAN',
                                )
-    annotations = []
     for i, row in input_df.iterrows():
-        annotations.append(dict(text=row['KECAMATAN'], xy=row['coords'], horizontalalignment='center'))
+        plt.annotate(text=row['KECAMATAN'], xy=row['coords'], horizontalalignment='center')
     choropleth.update_layout(
         plot_bgcolor='rgba(0, 0, 0, 0)',
         paper_bgcolor='rgba(0, 0, 0, 0)',
         margin=dict(l=0, r=0, t=0, b=0),
-        height=350,
-        annotations=annotations  # Add annotations to the layout
+        height=350,  # Add annotations to the layout
     )
     choropleth.update_geos(fitbounds="locations", visible=True)
     return choropleth
