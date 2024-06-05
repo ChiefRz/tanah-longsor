4ae5624cfe5f0b8f6588366d71cb53f1db346747
ChiefRz <151807591+ChiefRz@users.noreply.github.com>
2024-06-05 20:23:57 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 
 #######################
-# Page configuration
+# Pengaturan Halaman
 st.set_page_config(
     page_title="Tanah Longsor Dashboard",
     layout="wide",
@@ -33,8 +33,8 @@ with st.sidebar:
     
 ######################
 # Function
-def make_choropleth(input_df, input_js, input_id, input_columne):
-    choropleth = px.choropleth(input_df, geojson=input_js,
+def buat_choropleth(input_df, input_js, input_id, input_columne):
+    fig = px.choropleth(input_df, geojson=input_js,
                                       locations=input_id,
                                       color=input_columne,
                                       color_continuous_scale='Reds',
@@ -42,42 +42,38 @@ def make_choropleth(input_df, input_js, input_id, input_columne):
                                       labels={'KEJADIAN':'KEJADIAN'},
                                       hover_name='KECAMATAN',
                                )
-    choropleth.update_layout(
+    fig.update_layout(
         plot_bgcolor='rgba(0, 0, 0, 0)',
         paper_bgcolor='rgba(0, 0, 0, 0)',
         margin=dict(l=0, r=0, t=0, b=0),
         height=350,  # Add annotations to the layout
     )
-    choropleth.update_geos(fitbounds="locations", visible=True)
-    return choropleth
+    fig.update_geos(fitbounds="locations", visible=True)
+    return fig
     
-def tren(input):
-    # Kelompokkan data berdasarkan bulan per bulan
-    input["TANGGAL_KEJADIAN"] = pd.to_datetime(input["TANGGAL_KEJADIAN"])
-    data_bulan = input.groupby(input["TANGGAL_KEJADIAN"].dt.month)
+def buat_tren(input_df):
+    input_df["TANGGAL_KEJADIAN"] = pd.to_datetime(input_df["TANGGAL_KEJADIAN"])
+    data_bulan = input_df.groupby(input_df["TANGGAL_KEJADIAN"].dt.month)
     
-    # Hitung jumlah kejadian bencana yang unik pada kolom NO setiap bulan
     jumlah_kejadian = data_bulan["NO"].nunique()
     
-    # Ubah format indeks menjadi nama bulan
     nama_bulan = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
     jumlah_kejadian.index = [nama_bulan[i-1] for i in jumlah_kejadian.index]
     jumlah_kejadian = jumlah_kejadian.reindex(nama_bulan, fill_value=0)
 
-    # Ubah indeks menjadi kolom biasa dan membuat indeks baru
     jumlah_kejadian = jumlah_kejadian.reset_index()
     jumlah_kejadian = jumlah_kejadian.rename(columns={"index": "Bulan", "NO": "Jumlah Kejadian Bencana"})
     
-    tren = px.line(jumlah_kejadian, x='Bulan', y='Jumlah Kejadian Bencana', markers=True)
-    return tren
-    
+    fig = px.line(jumlah_kejadian, x='Bulan', y='Jumlah Kejadian Bencana', markers=True)
+    return fig
 
-def create_sum_order_items_df(df):
-    sum_order_items_df = df.groupby('KECAMATAN')['NO'].count().reset_index(name='JUMLAH_KEJADIAN')
-    return sum_order_items_df
+def buat_jumlah_kejadian():
+    sum_order_items_df = df_rekap_selected_year.groupby('KECAMATAN')['NO'].count().reset_index(name='JUMLAH_KEJADIAN')
+    df_selected_year_sorted = sum_order_items_df.sort_values(by="JUMLAH_KEJADIAN", ascending=False)
+    return df_selected_year_sorted
 
-def generate_chart(input):
-    rekap_lapor = input.groupby(by="PELAPOR").NO.nunique().reset_index()
+def buat_pelapor(input_df):
+    rekap_lapor = input_df.groupby(by="PELAPOR").NO.nunique().reset_index()
     rekap_lapor.rename(columns={
         "NO": "Jumlah Kejadian Bencana"
     }, inplace=True)
@@ -86,7 +82,7 @@ def generate_chart(input):
     fig.update_layout(legend=dict(orientation="h"))
     return fig
 
-def musim(input_df):
+def buat_musim(input_df):
     rekap_musim = input_df.groupby(by="MUSIM").NO.nunique().reset_index()
     rekap_musim.rename(columns={
          "NO": "Jumlah Kejadian Bencana"
@@ -97,27 +93,26 @@ def musim(input_df):
     return fig
 
 
-sum_order_items_df = create_sum_order_items_df(df_rekap_selected_year)
-df_selected_year_sorted = sum_order_items_df.sort_values(by="JUMLAH_KEJADIAN", ascending=False)
+
 #######################
-# Dashboard Main Panel
+# Dashboard
 col = st.columns((5, 2), gap='medium')
 
 with col[0]:
     st.markdown(f' #### Peta Sebaran Tanah Longsor Kab. Semarang pada Tahun {selected_year}')
     
-    choropleth = make_choropleth(df_peta_selected_year, df_peta_selected_year.geometry, df_peta_selected_year.index, 'KEJADIAN')
+    choropleth = buat_choropleth(df_peta_selected_year, df_peta_selected_year.geometry, df_peta_selected_year.index, 'KEJADIAN')
     st.plotly_chart(choropleth, use_container_width=True)
 
     st.markdown(f' #### Jumlah Kejadian Tanah Longsor Kab. Semarang Tahun {selected_year}')
     
-    tren = tren(df_rekap_selected_year)
+    tren = buat_tren(df_rekap_selected_year)
     st.plotly_chart(tren, use_container_width=True)
 
 with col[1]:
     st.markdown('#### Kecamatan Paling Sering Terjadi Bencana')
 
-    st.dataframe(df_selected_year_sorted,
+    st.dataframe(buat_jumlah_kejadian,
                  column_order=("KECAMATAN", "JUMLAH_KEJADIAN"),
                  hide_index=True,
                  width=None,
@@ -129,14 +124,14 @@ with col[1]:
                         "JUMLAH_KEJADIAN",
                         format="%f",
                         min_value=0,
-                        max_value=max(df_selected_year_sorted.JUMLAH_KEJADIAN),
+                        max_value=max(buat_jumlah_kejadian.JUMLAH_KEJADIAN),
                      )}
                  )
     
     st.markdown('##### Proporsi Pelaporan')
-    fig = generate_chart(df_rekap_selected_year)
-    st.plotly_chart(fig, use_container_width=True)
+    pelapor = buat_pelapor(df_rekap_selected_year)
+    st.plotly_chart(pelapor, use_container_width=True)
     
     st.markdown('##### Rekap Bencana Berdasarkan Musim')
-    fig = musim(df_rekap_selected_year)
-    st.plotly_chart(fig, use_container_width=True)
+    musim = buat_musim(df_rekap_selected_year)
+    st.plotly_chart(musim, use_container_width=True)
