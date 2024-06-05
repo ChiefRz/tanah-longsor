a700ae0d2d00ab74e5b2e888e1ff1a651d02716a
ChiefRz <151807591+ChiefRz@users.noreply.github.com>
2024-06-05 09:17:57 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,6 +37,8 @@ with st.sidebar:
 # Function
 # Choropleth map
 def make_choropleth(input_df, input_js, input_id, input_columne):
+    input_df['coords'] = input_df['geometry'].apply(lambda x: x.representative_point().coords[:])
+    input_df['coords'] = [coords[0] for coords in input_df['coords']]
     choropleth = px.choropleth(input_df, geojson=input_js,
                                       locations=input_id,
                                       color=input_columne,
@@ -45,11 +47,15 @@ def make_choropleth(input_df, input_js, input_id, input_columne):
                                       labels={'KEJADIAN':'KEJADIAN'},
                                       hover_name='KECAMATAN',
                                )
+    annotations = []
+    for i, row in input_df.iterrows():
+        annotations.append(dict(text=row['KECAMATAN'], xy=row['coords'], horizontalalignment='center'))
     choropleth.update_layout(
         plot_bgcolor='rgba(0, 0, 0, 0)',
         paper_bgcolor='rgba(0, 0, 0, 0)',
         margin=dict(l=0, r=0, t=0, b=0),
-        height=350
+        height=350,
+        annotations=annotations  # Add annotations to the layout
     )
     choropleth.update_geos(fitbounds="locations", visible=True)
     return choropleth
